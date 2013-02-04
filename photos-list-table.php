771a4ac25471e4b13d07ecc4e01c9272f542a4c4
Cyrille37 <cyrille37@gmail.com>
2013-02-04 14:38:41 +0100

    Photos List Table done.

--- photos-list-table.php
+++ photos-list-table.php
@@ -10,6 +10,13 @@ if (!class_exists('WP_List_Table')) {
  */
 class Photod_List_Table extends WP_List_Table {
 
+	const DEFAULT_ORDERBY = 'id' ; // 'photo_name';
+	const DEFAULT_ORDER = 'asc';
+	const DEFAULT_ITEMS_PER_PAGE = 5;
+	const DEFAULT_COLUMN_TYPE = 'str';
+
+	protected $columns;
+
 	function __construct() {
 
 		parent::__construct(array(
@@ -17,126 +24,83 @@ class Photod_List_Table extends WP_List_Table {
 			'plural' => __('photos'), //plural name of the listed records
 			'ajax' => false //does this table support ajax?
 		));
+
+		$this->columns = array(
+			'id' => array('label' => __('Id'), 'type'=>'int', 'hidden' => true),
+			'photo_name' => array('label' => __('Photo name')),
+			'photographer_name' => array('label' => __('Photographer name')),
+			'photographer_email' => array('label' => __('Photographer email'))
+		);
 	}
 
 	function get_columns() {
 
-		$columns = array(
-			'photo_name' => __('Photo name'),
-			'photographer_name' => __('Photographer name'),
-			'photographer_email' => __('Photographer email'),
-		);
+		$columns = array();
+		foreach ($this->columns as $k => $v) {
+			$columns[$k] = $v['label'];
+		}
 		return $columns;
 	}
 
 	function get_sortable_columns() {
-		$sortable_columns = array(
-			'photo_name' => array('photo_name', false), //true means it's already sorted
-			'photographer_name' => array('photographer_name', false),
-			'photographer_email' => array('photographer_email', false)
-		);
+		
+		$sortable_columns = array();
+		foreach ($this->columns as $k => $v) {
+			$sortable_columns[$k] = array( $k, $k==self::DEFAULT_ORDERBY ? true : false );
+		}
 		return $sortable_columns;
 	}
 
-	var $example_data = array(
-		array(
-			'ID' => 1,
-			'photo_name' => '300',
-			'photographer_email' => 'R',
-			'photographer_name' => 'Zach Snyder'
-		),
-		array(
-			'ID' => 2,
-			'photo_name' => 'Eyes Wide Shut',
-			'photographer_email' => 'R',
-			'photographer_name' => 'Stanley Kubrick'
-		),
-		array(
-			'ID' => 3,
-			'photo_name' => 'Moulin Rouge!',
-			'photographer_email' => 'PG-13',
-			'photographer_name' => 'Baz Luhrman'
-		),
-		array(
-			'ID' => 4,
-			'photo_name' => 'Snow White',
-			'photographer_email' => 'G',
-			'photographer_name' => 'Walt Disney'
-		),
-		array(
-			'ID' => 5,
-			'photo_name' => 'Super 8',
-			'photographer_email' => 'PG-13',
-			'photographer_name' => 'JJ Abrams'
-		),
-		array(
-			'ID' => 6,
-			'photo_name' => 'The Fountain',
-			'photographer_email' => 'PG-13',
-			'photographer_name' => 'Darren Aronofsky'
-		),
-		array(
-			'ID' => 7,
-			'photo_name' => 'Watchmen',
-			'photographer_email' => 'R',
-			'photographer_name' => 'Zach Snyder'
-		)
-	);
-
 	function column_default($item, $column_name) {
 
-		_log('$column_name: '. $column_name);
-		switch ($column_name) {
-			case 'photo_name':
-			case 'photographer_name':
-			case 'photographer_email':
-				return $item[$column_name];
+		$cType = self::DEFAULT_COLUMN_TYPE;
+		if( isset($this->columns[$column_name]['type']) )
+		{
+			$cType = $this->columns[$column_name]['type'];
+		}
+		switch ($cType) {
+			case 'int':
+			case 'str':
 			default:
-				return print_r($item, true); //Show the whole array for troubleshooting purposes
+				return $item[$column_name];
 		}
 	}
 
-	const DEFAULT_ORDERBY = 'photo_name' ;
-	
+	function get_hidden_columns() {
+		
+		$columns = array();
+		foreach ($this->columns as $k => $v) {
+			if( isset($this->columns[$k]['hidden']))
+				$columns[] = $k ;
+		}
+		return $columns;
+	}
+
 	function prepare_items() {
 
 		global $wpdb;
 
-		$per_page = 5;
+		$per_page = self::DEFAULT_ITEMS_PER_PAGE;
 
 		$columns = $this->get_columns();
-		$hidden = array();
+		$hidden = $this->get_hidden_columns();
 		$sortable = $this->get_sortable_columns();
 		$this->_column_headers = array($columns, $hidden, $sortable);
 
 		$current_page = $this->get_pagenum();
 
+		$dataCount = $wpdb->get_var( 'SELECT COUNT(*) FROM '.AefPhotosContest::$dbtable_photos );
+
 		$data = $wpdb->get_results(
-			'SELECT * FROM ' . AefPhotosContest::$dbtable_photos
-			.' ORDER BY '.(!empty($_REQUEST['orderby']) ? $_REQUEST['orderby'] : self::DEFAULT_ORDERBY)
-			.' '.(!empty($_REQUEST['order']) && ( $_REQUEST['order']=='asc' || $_REQUEST['order']=='desc') ? $_REQUEST['order'] : 'ASC')
+			'SELECT '. implode(',',array_keys($this->columns) ) .' FROM ' . AefPhotosContest::$dbtable_photos
+			. ' ORDER BY ' . (!empty($_REQUEST['orderby']) ? $_REQUEST['orderby'] : self::DEFAULT_ORDERBY)
+			. ' ' . (!empty($_REQUEST['order']) && ( $_REQUEST['order'] == 'asc' || $_REQUEST['order'] == 'desc') ? $_REQUEST['order'] : self::DEFAULT_ORDER)
 			. ' LIMIT ' . $per_page . ' OFFSET ' . (($current_page - 1) * $per_page)
 			, ARRAY_A);
 
-		/*
-		$data = $this->example_data;
-
-       function usort_reorder($a,$b){
-            $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'title'; //If no sort, default to title
-            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
-            $result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
-            return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
-        }
-        usort($data, 'usort_reorder');
-		$data = array_slice($data, (($current_page - 1) * $per_page), $per_page);
- */
-
 		$this->items = $data;
-		$total_items = 7 ; //count($data);
+		$total_items = $dataCount;
 
-		/**
-		 * REQUIRED. We also have to register our pagination options & calculations.
-		 */
 		$this->set_pagination_args(array(
 			'total_items' => $total_items, //WE have to calculate the total number of items
 			'per_page' => $per_page, //WE have to determine how many items to show on a page
